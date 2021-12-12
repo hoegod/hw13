46a82b3a536a394c811cdb9ce268c6d80dd519dd
t110AE5003 劉淦皓 <79521625+hoegod@users.noreply.github.com>
2021-12-12 22:24:34 +0800

    Update MainActivity.kt

--- app/src/main/java/com/italkutalk/lab13/MainActivity.kt
+++ app/src/main/java/com/italkutalk/lab13/MainActivity.kt
@@ -10,10 +10,10 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    //建立 BroadcastReceiver 物件
+
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            //接收廣播後，解析 Intent 取得字串訊息
+
             intent.extras?.let {
                 val tv_msg = findViewById<TextView>(R.id.tv_msg)
                 tv_msg.text = "${it.getString("msg")}"
@@ -34,14 +34,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
     override fun onDestroy() {
-        unregisterReceiver(receiver) //註銷廣播
+        unregisterReceiver(receiver) 
         super.onDestroy()
     }
     private fun register(channel: String) {
-        //建立 IntentFilter 物件來指定接收的頻道，並註冊 Receiver
+
         val intentFilter = IntentFilter(channel)
         registerReceiver(receiver, intentFilter)
-        //建立 Intent 物件，使其夾帶頻道資料，並啟動 MyService 服務
+
         val i = Intent(this, MyService::class.java)
         startService(i.putExtra("channel", channel))
     }
